afbbb89b6fd69e4bdac5c61fcded8949fd012e83
Trang2910 <trangnt72@wru.vn>
2019-09-22 14:55:28 +0700

    new php

---
+++ bottom.php
@@ -0,0 +1,78 @@
+        <div id="footer ">
+             
+            <div class="row fnav pb-4  " style="background-color: #024282; margin-right: 0px; margin-left: 0px">
+                <div class="col-lg-3 col-sm-6" style="padding-left: 40px; padding-right: 40px ; padding-top: 30px">
+                    <p style="padding-left: 40px; padding-right: 40px "> <a href="#" id="mau1"><b >ĐH NGUYỄN TẤT THÀNH</b></a></p>
+                                            <ul class="mau">
+                                                            <li><a href="#">Thư ngỏ</a></li>
+                                                            <li><a href="#">Tầm nhìn - Sứ mạng</a></li>
+                                                            <li><a href="">Văn hóa ĐH Nguyễn Tất Thành</a></li>
+                                                            <li><a href="">Thông tin 3 công khai</a></li>
+                                                            <li><a href="">Đảm bảo chất lượng</a></li>
+                                                            <li><a href="">Phát triển bền vững</a></li>
+                                                            <li><a href="">Trường Trung cấp Nguyễn Tất Thành</a></li>
+                                                            <li><a href="">Trường Tiểu học Anh Việt Mỹ</a></li>
+                                                    </ul>
+                                    </div>
+
+                <div class="col-lg-3 col-sm-6"style="padding-left: 40px; padding-right: 40px ; padding-top: 30px">
+                    <p><a href="" id="mau1"><b >PHÒNG - BAN</b></a></p>
+                                        <br>
+                    <p><a href="" id="mau1"><b>THƯ VIỆN ẢNH</b></a></p>
+                    <br>
+                    <p><a href="" id="mau1"><b>E-LEARNING</b></a></p>
+                    
+                </div>
+
+                <div class="col-lg-3 col-sm-6" style="padding-left: 40px; padding-right: 40px ; padding-top: 30px">
+                    <p style="padding-left: 40px; padding-right: 40px "><a href="#" id="mau1"><b>TRUNG TÂM - VIỆN</b></a></p>
+                                            <ul class="mau">
+                                                            <li><a href="">Viện Đào Tạo Quốc Tế</a></li>
+                                                            <li><a href="">Viện Công Nghệ Cao</a></li>
+                                                            <li><a href="">Viện Đào tạo Sau đại học</a></li>
+                                                            <li><a href="">Trung tâm Thông tin Thư viện</a></li>
+                                                            <li><a href="">TT Ngoại Ngữ</a></li>
+                                                            <li><a href="">TT Tin Học NTT</a></li>
+                                                            <li><a href="">TT QH Doanh Nghiệp &amp; Khởi nghiệp</a></li>
+                                                            <li><a href="">TT Đào tạo theo nhu cầu XH</a></li>
+                                                            <li><a href="">TT Xuất khẩu lao động Texgamex</a></li>
+                                                            <li><a href="">Công đoàn Trường ĐH Nguyễn Tất Thành</a></li>
+                                                    </ul>
+                                    </div>
+                <div class="col-lg-3 col-sm-6" style="padding-left: 40px; padding-right: 40px ; padding-top: 30px">
+                    <ul class="mau">
+                        <li><a href="" class="btn"><i class="fab fa-facebook-square"></i> Facebook</a></li>
+                        <li><a href="" class="btn"><i class="fab fa-google-plus"></i> Google Plus</a></li>
+                        <li><a href="" class="btn"><i class="fab fa-youtube"></i> Youtube</a></li>
+                        <li><a href="#" class="btn"><i class="fas fa-comments"></i> Email</a></li>
+                        <li><a href="#" class="btn"><i class="fas fa-comments"></i> Zalo</a></li>
+                        <li><a href="" class="btn"><i class="fas fa-graduation-cap"></i> Egov</a></li>
+                        <li><a href="" class="btn"><i class="fa fa-users"></i>Tuyển dụng</a></li>
+                    </ul>
+                </div>
+
+            </div>
+                <br>
+            <div class="copyright">
+                <div class="container">
+                    <div class="row pb-2">
+                        <div class="col-md-7">
+                            <p class="pb-0">
+                                <b>THÔNG TIN LIÊN HỆ </b><br>
+                        Trụ sở chính: <b>300A – Nguyễn Tất Thành, Phường 13, Quận 4, TP. Hồ Chí Minh, Việt Nam</b> <br>
+                    Điện thoại: <b> 1900 2039 </b>      Fax: <b>028 39 404 759</b><br>
+                Hotline: <b>0902 298 300 – 0906 298 300 – 0912 298 300 – 0914 298 300</b><br>
+            Email: <b>tttvtsinh@ntt.edu.vn – bangiamhieu@ntt.edu.vn </b>
+    
+                            </p>
+                            
+                        </div>
+                        <div class="col-md-5"><p class="">©2017. Bản quyền thuộc về trường <b>Đại học Nguyễn Tất Thành</b></p> </div>
+                                    </div>
+                    
+                                    <!-- /container --></div>
+                            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ top.php
@@ -0,0 +1,116 @@
+
+<body>
+    <div id="wrapper">
+        <div id="header">
+
+            <div id="menu1">
+                <nav class="navbar navbar-expand-lg navbar-dark" style="padding: 0px;">
+                              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                              </button>
+
+                              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <a class="nav-link" href="#" style="color: #fdb913; margin-left: 30px;">HOTLINE: 0912.336.069  -  0906.149.686</a>
+                                <ul class="navbar-nav mr-auto">
+                                  <li class="nav-item">
+                                    <a class="nav-link nav1" href="Sinhvien.html" >SINH VIÊN</a>
+                                  </li>
+                                  <li class="nav-item">
+                                    <a class="nav-link nav1" href="Giangvien.html" >GIẢNG VIÊN</a>
+                                  </li>
+                                  <li class="nav-item">
+                                    <a class="nav-link nav1" href="#" >ĐÀO TẠO QUỐC TẾ</a>
+                                  </li>
+                                </ul>
+                              </div>
+                </nav>
+                <form class="navbar-form navbar-left" role="search">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Tìm kiếm thông tin">
+                        <span class="input-group-btn">
+                            <i class="fas fa-search" style="color: white; padding:15px;"></i>
+                        </span>
+                    </div>
+                </form>
+                </div>
+            </div>
+
+            <div id="menu2" class="container-fluid">
+                <div id="logo" class="col-3">
+                    <a href="index.php"><img src="http://ntt.edu.vn/web/frontend/images/logo_ntt.png" alt="logo"></a>
+                </div>
+                <div id="nav2" class="col-9">
+                    <nav class="nav nav-pills nav-fill">
+                      <a class="nav-item nav-link nav2" href="Gioithieu.php"><i class="fa fa-info-circle"></i> Giới thiệu</a>
+                      <a class="nav-item nav-link nav2" href="Tuyensinh.php"><i class="fa fa-graduation-cap"></i> Tuyển sinh</a>
+                      <a class="nav-item nav-link nav2" href="Daotao.php"><i class="fa fa-book"></i> Đào tạo</a>
+                      <a class="nav-item nav-link nav2" href="Nghiencuu.php"><i class="fa fa-bookmark"></i> Nghiên cứu</a>
+                      <a class="nav-item nav-link nav2" href="Hoptacdoanhnghiep.php"><i class="fas fa-handshake"></i> Hợp tác doanh nghiêp</a>
+                    </nav>
+                </div>
+            </div>
+            
+            <div id="menu3" >
+                        
+                        <div class="dropdown" style="position: relative;">
+                            <span class="span" style="z-index: 2;top: -1px; right: 46%;position: absolute; " data-toggle="dropdown"><i id="triangle-button" class="fa fa-bars" style="font-size: 30px;"></i></span>
+                            
+                            <button type="button" class="btn btn-responsive" data-toggle="dropdown" style=""><i class="fa fa-bars" style="font-size: 30px;"></i></button>
+
+                            <div class="dropdown-menu" style="width: 100%;z-index: 1;">
+                                <div class="container" style="padding: 0px; margin: 0px;">
+                                    <div class="row menu-main" style="padding-top: 50px;">
+                                        <div class="col-3">
+                                            <h5 class="dropdown-header" style="color: #fff">Giới thiệu chung</h5>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Giới thiệu chung</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Toàn cảnh ĐH Nguyễn Tất Thành</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Thông tin 3 công khai</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Đảm bảo chất lượng</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Phát triển bền vững</a>
+
+                                            <h5 class="dropdown-header" style="color: #fff">Hợp tác doanh nghiệp</h5>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Giới thiệu việc làm</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Giới thiệu thực tập</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Câu lạc bộ Doanh nghiệp</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Ban liên lạc Cựu sinh viên</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Liên kết hợp tác</a>
+                                        </div>
+                                        <div class="col-3">
+                                            <h5 class="dropdown-header" style="color: #fff">Tuyển sinh</h5>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Các bậc đào tạo</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Thông tin tuyển sinh</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Chính sách học bổng</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Hướng nghiệp</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Xét tuyển trực tuyên</a>
+
+                                            <h5 class="dropdown-header" style="color: #fff">Nghiên cứu</h5>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Hoạt động khoa học công nghệ</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Viện-Trung tâm nghiên cứu</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Công bố khoa học</a>
+                                        </div>
+                                        <div class="col-3">
+                                            <h5 class="dropdown-header" style="color: #fff">Đào tạo</h5>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Khối khoa học sức khỏe</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Khối Kinh tế-Quản trị</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Khối Kỹ thuật-Công nghệ</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Khối Mỹ thuật-Nghệ thuật</a>
+                                            <a class="dropdown-item" href="#"><i class="fa fa-angle-double-right"></i> Khối đào tạo quốc tế</a>
+
+                                            
+                                        </div>
+                                        <div class="col-3 menu-icon">
+                                            <br>
+                                            <a class="dropdown-item" href="#"><i class="fab fa-facebook-square"></i> Facebook</a>
+                                            <a class="dropdown-item" href="#"><i class="fab fa-google-plus"></i> Google</a>
+                                            <a class="dropdown-item" href="#"><i class="fab fa-youtube"></i> Youtube</a>
+                                            <a class="dropdown-item" href="#"><i class="fas fa-envelope"></i> Email</a>
+                                            <a class="dropdown-item" href="#"><i class="fas fa-comments"></i> Zalo</a>
+                                            <a class="dropdown-item" href="#"><i class="fas fa-graduation-cap"></i> Egov</a>
+                                            <a class="dropdown-item" href="#"><i class="fas fa-users"></i> Tuyển dụng</a>
+                                        </div>
+                                    </div>
+                                </div>
+                              
+                              
+                            </div>
+                        </div>
